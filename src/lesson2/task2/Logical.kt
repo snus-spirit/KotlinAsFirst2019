d7c3ad729682e04bb9e322a32d8e5074e2244d9d
snus-spirit <macum001@mail.ru>
2019-09-18 23:36:30 +0300

    fixed

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -40,7 +40,7 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
 fun daysInMonth(month: Int, year: Int): Int {
-    if ((month == 2) && (((year % 100 != 0) || (year % 400 == 0)) || (year % 4 == 0) || (year % 400 == 0))) return 29
+    if ((month == 2) && ((year % 100 != 0) || (year % 400 == 0)) && ((year % 4 == 0) || (year % 400 == 0))) return 29
     return when (month) {
         1, 3, 5, 7, 8, 10, 12 -> 31
         2 -> return 28
